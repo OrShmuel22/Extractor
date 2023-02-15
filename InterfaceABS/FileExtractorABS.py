23233772b556a65f29fd583dd1956126ae5ec8d6
or <or@gmail>
2023-02-15 22:33:08 +0200

    Add FileExtractorABS

--- InterfaceABS/FileExtractorABS.py
+++ InterfaceABS/FileExtractorABS.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class FileExtractor(ABC):
     @abstractmethod
-    def extract_file(self, filename, path_to_extract):
+    def extract_file(self, compressed_file_path, path_to_uncompressed_file):
         pass
--- extractor_handler.py
+++ extractor_handler.py
@@ -1,4 +1,6 @@
+import io
 import logging
+import magic
 from InterfaceABS.MicroServiceABS import Microservice
 from utils.rabbitqAdapter import RabbitMQMessageBroker
 
@@ -6,7 +8,7 @@ from utils.rabbitqAdapter import RabbitMQMessageBroker
 class MyExtractor(Microservice):
     def __init__(self, message_broker):
         self.message_broker = message_broker
-        # Todo get it from configuration file with different types of extractor we have
+        # Todo: get it from configuration file with different types of extractor we have
         self.extractors = {}
 
     def start(self):
@@ -20,6 +22,15 @@ class MyExtractor(Microservice):
     def execute(self, message):
         # Add your message extraction logic here
         logging.info(f"Received file: {message}")
+        file_type = self.get_file_type(message)
+
+    # TODO: Move this function to "global util" it can be used for different purposes
+    @staticmethod
+    def get_file_type(file_path):
+        # recommend using at least the first 2048 bytes, as less can produce incorrect identification
+        file_type = magic.from_buffer(open(file_path, "rb").read(2048), mime=True)
+
+        return file_type
 
 
 if __name__ == '__main__':
